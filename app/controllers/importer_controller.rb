81c2dba6d46d9b00ae83c9029ed2f23aee5266d2
Cyril Lepagnot <cyril.lepagnot@webpulser.com>
2011-11-28 16:50:39 +0100

    fix story_points update in importer

--- app/controllers/importer_controller.rb
+++ app/controllers/importer_controller.rb
@@ -190,7 +190,6 @@ class ImporterController < ApplicationController
       issue.project_id = project != nil ? project.id : @project.id
       issue.tracker_id = tracker != nil ? tracker.id : default_tracker
       issue.author_id = author != nil ? author.id : User.current.id
-      issue.story_points = row[attrs_map["story_points"]] != nil ? row[attrs_map["story_points"]].to_i : nil if attrs_map["story_points"]
 
       # trnaslate unique_attr if it's a custom field -- only on the first issue
       if !unique_attr_checked
@@ -262,6 +261,7 @@ class ImporterController < ApplicationController
       issue.fixed_version_id = fixed_version != nil ? fixed_version.id : issue.fixed_version_id
       issue.done_ratio = row[attrs_map["done_ratio"]] || issue.done_ratio
       issue.estimated_hours = row[attrs_map["estimated_hours"]] || issue.estimated_hours
+      issue.story_points = row[attrs_map["story_points"]].to_i || issue.story_points if attrs_map["story_points"]
 
       # parent issues
       begin
